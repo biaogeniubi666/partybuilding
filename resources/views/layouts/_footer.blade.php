cf1d2da11bf35368ba98a2d81c46bca821e65e76
biaogeniubi666 <365056996@qq.com>
2019-09-11 15:51:53 +0800

    前端页面测试

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,15 +1,3 @@
-{{-- <footer class="footer">
-    <div class="container">
-      <p class="float-left">
-        由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Biaoge</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
-      </p>
-  
-      <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
-    </div>
-  </footer> --}}
-
-
-
 <div class="row" style="padding-right: 0px; padding-left: 0px; margin-left: 0px; margin-right: 0px;">
   <div class="col-md-12" style="text-align:center">
     <p class="text-primary" style="padding-bottom: 10px; padding-top: 10px;  margin-top: 5px;">由 中睿智慧应用研究院 设计与编码 </p>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,29 +1,28 @@
 <div class="clearfix visible-xs-block"></div>
-  <div class="row" style="padding-right: 0px; padding-left: 0px;margin-left: 0px;margin-right: 0px;">
-      <div class="col-md-12">
-        <div class="head">
-          <h1>Owell智慧健康平台</h1>
-          <div class="weather"><!--<img src="picture/weather.png"><span>多云转小雨</span>--><span id="showTime"></span></div>
-              
-          {{-- 北京时间,有空再整 --}}
-          <script>
-            var t = null;
-                t = setTimeout(time,1000);//開始运行
-                function time()
-                  {
-                  clearTimeout(t);//清除定时器
-                  dt = new Date();
-                      var y=dt.getFullYear();
-                      var mt=dt.getMonth()+1;
-                      var day=dt.getDate();
-                  var h=dt.getHours();//获取时
-                  var m=dt.getMinutes();//获取分
-                  var s=dt.getSeconds();//获取秒
-                  document.getElementById("showTime").innerHTML = y+"年"+mt+"月"+day+"-"+h+"时"+m+"分"+s+"秒";
-                  t = setTimeout(time,1000); //设定定时器，循环运行     
-                } 
-          </script>
-        </div>
+<div class="row" style="padding-right: 0px; padding-left: 0px;margin-left: 0px;margin-right: 0px;">
+    <div class="col-md-12">
+      <div class="head">
+        <h1>Owell智慧健康平台</h1>
+        <div class="weather"><!--<img src="picture/weather.png"><span>多云转小雨</span>--><span id="showTime"></span></div>
+            
+        {{-- 北京时间,有空再整 --}}
+        <script>
+          var t = null;
+              t = setTimeout(time,1000);//開始运行
+              function time()
+                {
+                clearTimeout(t);//清除定时器
+                dt = new Date();
+                    var y=dt.getFullYear();
+                    var mt=dt.getMonth()+1;
+                    var day=dt.getDate();
+                var h=dt.getHours();//获取时
+                var m=dt.getMinutes();//获取分
+                var s=dt.getSeconds();//获取秒
+                document.getElementById("showTime").innerHTML = y+"年"+mt+"月"+day+"-"+h+"时"+m+"分"+s+"秒";
+                t = setTimeout(time,1000); //设定定时器，循环运行     
+              } 
+        </script>
       </div>
-  </div>
+    </div>
 </div>
