72c1870991bb5192330151b87bc1c88de242363b
biaogeniubi666 <365056996@qq.com>
2019-09-26 22:51:22 +0800

    555555555

--- app/Helpers/Python/test_airdata.py
+++ app/Helpers/Python/test_airdata.py
@@ -9,58 +9,6 @@ encoding = 'utf-8'
 BUFSIZE = 1024
 
 def go ():
-    class Reader(threading.Thread):
-        def __init__(self, client):
-            threading.Thread.__init__(self)
-            self.client = client
-        def run(self):
-            while True:
-                data = self.client.recv(BUFSIZE)
-                if (data):
-                    string = bytes.decode(data, encoding)
-                    #   print(string)
-                    strs = str(string)
-                    print(strs)
-                    print("----------------------------")
-                    d={}
-                    connection = pymysql.connect(host="47.96.18.108",port=3306,user="root",passwd="root",db="default" )
-                    #   wangzhi="https://www.jb51.net/article/148183.htm"
-                    Clientid=strs[40:49]
-                    strs_for_json = strs[55:]
-                    strs_for_json= strs_for_json[:-9]
-                    print(strs_for_json)
-                    s1=strs_for_json.split(";")
-                    print(s1)
-                    print("----------------------------")
-                    d['Clientid']=Clientid
-                    for s2 in s1 :
-                        s3=s2.split("=",2)
-                        key=s3[0][:-4]
-                        value=s3[1]
-                        d[key]=value
-                    print (d)
-                    print("----------------------------")
-                    data_json=d
-                    cursor_ser = connection.cursor()
-                    cursor_ser.execute("INSERT INTO airs (co2,voc,tem,hum,pm25,ch2o) VALUES ('%s','%s','%s','%s','%s','%s')" % (data_json['CO2'],data_json['VOC'],data_json['TEMP'],data_json['HUMI'],data_json['PM25'],data_json['CH2O']))
-                    connection.commit()
-                    connection.close()
-                    time.sleep(60)
-                else:
-                    break
-            print("close:", self.client.getpeername() )
-        def readline(self):
-            rec =self.client.readline()
-            if rec:
-                string = bytes.decode(rec, encoding)
-                if len(string)>2:
-                    string = string[0:-2]
-                else:
-                    string = ' '
-            else:
-                string = False
-            return string
-
     class Listener(threading.Thread):
         def __init__(self, port):
             threading.Thread.__init__(self)
@@ -74,10 +22,10 @@ def go ():
             print("listener started")
             while True:
                 client, cltadd = self.sock.accept()
-                Reader(client).start()
-                cltadd = cltadd
-
                 print(cltadd)
+                print("----------------------")
+                print(client)
+                print("----------------------")
                 print("accept a connect")
     lst  = Listener(11111)   # create a listen thread
     lst.start() # then start
