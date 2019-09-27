6ba7eba54fcd701c492af912a91499e9b79c2093
biaogeniubi666 <365056996@qq.com>
2019-09-27 09:47:37 +0800

    11111端口service测试失败，做其他的事儿

--- app/Helpers/Python/process.py
+++ app/Helpers/Python/process.py
@@ -15,9 +15,6 @@ import numpy
 import php_python
 import test_socket  #具体的python脚本模块
 
-import php_python_2 #自己加的
-import test_airdata #自己加的
-
 REQUEST_MIN_LEN = 10    #合法的request消息包最小长度    
 TIMEOUT = 180           #socket处理时间180秒
 
