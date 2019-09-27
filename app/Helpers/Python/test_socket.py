7519699714f6df3d666f15259350dee2b98b391c
biaogeniubi666 <365056996@qq.com>
2019-09-27 09:14:46 +0800

    11111   2

--- app/Helpers/Python/test_socket.py
+++ app/Helpers/Python/test_socket.py
@@ -5,8 +5,7 @@ import threading
 import socket
 
 def  go(msg): 
-    
-    print(msg)   
+
     try:
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         s.connect(('47.96.18.108',12346))
