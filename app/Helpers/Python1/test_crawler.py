da9ff23f0c73a2ed6a90f5271d129196ae395723
biaogeniubi666 <365056996@qq.com>
2019-09-27 14:43:42 +0800

    123

--- app/Helpers/Python1/test_crawler.py
+++ app/Helpers/Python1/test_crawler.py
@@ -16,35 +16,35 @@ class Reader(threading.Thread):
           while True:
              data = self.client.recv(BUFSIZE)
              if (data):
-                  string = bytes.decode(data, encoding)
-                #   print(string)
-                  strs = str(string)
-                  print(strs)
-                  print("----------------------------")
-                  d={}
-                  connection = pymysql.connect(host="47.96.18.108",port=3306,user="root",passwd="root",db="default" )
-                #   wangzhi="https://www.jb51.net/article/148183.htm"
-                  Clientid=strs[40:49]
-                  strs_for_json = strs[55:]
-                  strs_for_json= strs_for_json[:-9]
-                  print(strs_for_json)
-                  s1=strs_for_json.split(";")
-                  print(s1)
-                  print("----------------------------")
-                  d['Clientid']=Clientid
-                  for s2 in s1 :
-                      s3=s2.split("=",2)
-                      key=s3[0][:-4]
-                      value=s3[1]
-                      d[key]=value
-                  print (d)
-                  print("----------------------------")
-                  data_json=d
-                  cursor_ser = connection.cursor()
-                  cursor_ser.execute("INSERT INTO airs (co2,voc,tem,hum,pm25,ch2o) VALUES ('%s','%s','%s','%s','%s','%s')" % (data_json['CO2'],data_json['VOC'],data_json['TEMP'],data_json['HUMI'],data_json['PM25'],data_json['CH2O']))
-                  connection.commit()
-                  connection.close()
-                  time.sleep(60)
+                string = bytes.decode(data, encoding)
+            #   print(string)
+                strs = str(string)
+                print(strs)
+                print("----------------------------")
+                d={}
+                connection = pymysql.connect(host="47.96.18.108",port=3306,user="root",passwd="root1",db="partybuilding" )
+            #   wangzhi="https://www.jb51.net/article/148183.htm"
+                Clientid=strs[40:49]
+                strs_for_json = strs[55:]
+                strs_for_json= strs_for_json[:-9]
+                print(strs_for_json)
+                s1=strs_for_json.split(";")
+                print(s1)
+                print("----------------------------")
+                d['Clientid']=Clientid
+                for s2 in s1 :
+                    s3=s2.split("=",2)
+                    key=s3[0][:-4]
+                    value=s3[1]
+                    d[key]=value
+                print (d)
+                print("----------------------------")
+                data_json=d
+                cursor_ser = connection.cursor()
+                cursor_ser.execute("INSERT INTO airs (co2,voc,tem,hum,pm25,ch2o) VALUES ('%s','%s','%s','%s','%s','%s')" % (data_json['CO2'],data_json['VOC'],data_json['TEMP'],data_json['HUMI'],data_json['PM25'],data_json['CH2O']))
+                connection.commit()
+                connection.close()
+                time.sleep(60)
              else:
                   break
           print("close:", self.client.getpeername() )
