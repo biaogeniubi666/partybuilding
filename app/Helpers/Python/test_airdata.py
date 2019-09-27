665570e810aa6ff9aa93e1725fb2e95c42b23ed2
biaogeniubi666 <365056996@qq.com>
2019-09-27 09:20:10 +0800

    11111 3

--- app/Helpers/Python/test_airdata.py
+++ app/Helpers/Python/test_airdata.py
@@ -3,7 +3,7 @@ def  go():
    
     try:
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect(('172.16.85.52',11111))
+        s.connect(('47.96.18.108',11111))
         print('正在接收控制数据') 
         print(s.recv(1024))
     except:
