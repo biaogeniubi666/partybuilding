d3d1f8e68302593afb20bb8ec3eaf1b607ae36fe
biaogeniubi666 <365056996@qq.com>
2019-10-21 18:19:55 +0800

    redirect

--- app/Http/Controllers/LedsController.php
+++ app/Http/Controllers/LedsController.php
@@ -23,6 +23,8 @@ class LedsController extends Controller
         
         // python2为本地docker测试端口
         // ppython2("test_socket::go" , "conled" . $con_data);
+
+        return redirect('dataview/show');
     }
 
     public function update(){
